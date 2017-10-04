b5597e6a0dc6ce87f8a85424d528022c53661622
Daniel Luna <daniellunsc@gmail.com>
2017-10-04 10:52:48 -0300

    Adicao do BackGround

--- src/Components/FormCadastro.js
+++ src/Components/FormCadastro.js
@@ -1,39 +1,41 @@
 import React from 'react';
-import {Text, View, TextInput, Button} from 'react-native';
+import {Text, View, TextInput, Button, Image} from 'react-native';
 import {connect} from 'react-redux';
 import { CHANGE_EMAIL, CHANGE_PWD, CHANGE_NAME } from '../actions/AuthActions'
 
 const FormCadastro = props => {
     return(
-        <View style = {{flex:1, padding:10}}>
-            <View style = {{flex:4, justifyContent: 'center'}}>
-                <TextInput 
-                value={props.nome} 
-                placeholder="Nome"  
-                style={{fontSize:20, height:45}}
-                onChangeText={texto => props.CHANGE_NAME(texto)}/>
+        <Image style={{flex:1, width: null}} source={require('../imgs/bg.png')}>
+            <View style = {{flex:1, padding:10}}>
+                <View style = {{flex:4, justifyContent: 'center'}}>
+                    <TextInput 
+                    value={props.nome} 
+                    placeholder="Nome"  
+                    style={{fontSize:20, height:45}}
+                    onChangeText={texto => props.CHANGE_NAME(texto)}/>
 
-                <TextInput 
-                
-                value={props.email} 
-                placeholder="Email" 
-                style={{fontSize:20, height:45}}
-                onChangeText={texto => props.CHANGE_EMAIL(texto)}/>
+                    <TextInput 
+                    
+                    value={props.email} 
+                    placeholder="Email" 
+                    style={{fontSize:20, height:45}}
+                    onChangeText={texto => props.CHANGE_EMAIL(texto)}/>
 
-                <TextInput 
-                
-                secureTextEntry={true}
-                value={props.senha} 
-                placeholder="Senha" 
-                style={{fontSize:20, height:45}}
-                onChangeText={texto => props.CHANGE_PWD(texto)}/>
+                    <TextInput 
 
+                    secureTextEntry={true}
+                    value={props.senha} 
+                    placeholder="Senha" 
+                    style={{fontSize:20, height:45}}
+                    onChangeText={texto => props.CHANGE_PWD(texto)}/>
+
+                </View>
+
+                <View style = {{flex:1}}>
+                    <Button title="Cadastrar" color="#115E54" onPress={()=>false} />
+                </View>
             </View>
-
-            <View style = {{flex:1}}>
-                <Button title="Cadastrar" color="#115E54" onPress={()=>false} />
-            </View>
-        </View>
+        </Image>
     );
 }
 
--- src/Components/FormLogin.js
+++ src/Components/FormLogin.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, View, TextInput, Button,  TouchableHighlight } from 'react-native';
+import {Text, View, TextInput, Button,  TouchableHighlight, Image } from 'react-native';
 import { Actions } from 'react-native-router-flux'
 import { connect } from 'react-redux'
 import { CHANGE_EMAIL, CHANGE_PWD } from '../actions/AuthActions'
@@ -8,32 +8,34 @@ const formLogin = props =>
 {
 
         return (
-            <View style={{ flex:1, padding:10}}>
-                <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
-                    <Text style={{fontSize:25}}>WhatsApp Clone</Text>
-                </View>
-                <View style={{flex:2}}>
+            <Image style={{flex:1, width: null}} source={require('../imgs/bg.png')}>
+                <View style={{ flex:1, padding:10}}>
+                    <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
+                        <Text style={{fontSize:25}}>WhatsApp Clone</Text>
+                    </View>
+                    <View style={{flex:2}}>
 
-                    <TextInput value={props.email} 
-                    style={{fontSize:20, height:45}}
-                    placeholder="E-mail"
-                    onChangeText = {texto => props.CHANGE_EMAIL(texto)}/>
+                        <TextInput value={props.email} 
+                        style={{fontSize:20, height:45}}
+                        placeholder="E-mail"
+                        onChangeText = {texto => props.CHANGE_EMAIL(texto)}/>
 
-                    <TextInput 
-                    secureTextEntry={true}
-                    value={props.senha}
-                    style={{fontSize:20, height:45}}
-                    placeholder="Senha" 
-                    onChangeText = {texto => props.CHANGE_PWD(texto)}/>
+                        <TextInput 
+                        secureTextEntry={true}
+                        value={props.senha}
+                        style={{fontSize:20, height:45}}
+                        placeholder="Senha" 
+                        onChangeText = {texto => props.CHANGE_PWD(texto)}/>
 
-                <TouchableHighlight onPress={() => Actions.formCadastro() }>
-                    <Text style={{ fontSize: 20 }}>Ainda não tem cadastro? Cadastre-se</Text>
-                </TouchableHighlight>
+                    <TouchableHighlight onPress={() => Actions.formCadastro() }>
+                        <Text style={{ fontSize: 20 }}>Ainda não tem cadastro? Cadastre-se</Text>
+                    </TouchableHighlight>
+                    </View>
+                    <View style={{flex:2}}>
+                        <Button title="Acessar" color='#115E54' onPress={()=> false}/>
+                    </View>
                 </View>
-                <View style={{flex:2}}>
-                    <Button title="Acessar" color='#115E54' onPress={()=> false}/>
-                </View>
-            </View>
+            </Image>
         );
 }
 
